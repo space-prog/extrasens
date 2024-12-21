b941cf78fe432116379b5bf4913c2a7d8db7c77b
space-prog <p87717891@gmail.com>
2024-12-21 14:59:08 +0200

    sdf

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-</html>
+
 <?php
     session_start();
     // session_destroy();
@@ -12,37 +12,46 @@
 </head>
 <body>
     <?php
-
-        if (isset($_SESSION["rand"]) === false) {
-            $_SESSION["rand"] = rand(0, 10);
+        if(!isset($_SESSION['rand'])) {
+            $rand = $_SESSION['rand'] = rand(1,10);
+        } else {
+            $rand = $_SESSION['rand'];
         }
 
+        if (!isset($_SESSION["attempts"])) {
+            $attempts = $_SESSION['attempts'] = 0;
+        } else {
+            $attempts = $_SESSION["attempts"];
+        }
+        
         if (isset($_POST['choise'])) {
             $choise = $_POST['choise'];
             if ($choise == $_SESSION['rand']) {
-                echo "You win";
+                echo "You win, ";
             } else {
-                echo "try again";
+                echo "try again, ";
             }
-            $attempts = $_POST['attempts'];
-            $attempts++;
-            echo $attempts;
-        }
-        
-        if (isset($_POST['choise'])) {
-            $attempts++;
+            ++$_SESSION["attempts"];
+            echo $_SESSION['attempts'] . " спроб використано";
         }
         
     ?>
-    <form action="index.php">
+    <form action="
+        <?php
+            if($_SESSION['attempts'] >= 3) {
+                echo "results.php";
+            } else {
+                echo "index.php";
+            }
+        ?>
+    " method="post"> 
         <select name="choise">
             <?php
-                for($i = 0; $i<=10; $i++) {
+                for($i = 1; $i<=10; $i++) {
                     echo "<option value='$i'>$i</option>";
                 }
             ?>
         </select>
-        <input type='hidden' name='attempts' value='0'>
         <button type="submit">Перевірка</button>
     </form>
 </body>
